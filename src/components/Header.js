a36a390e255e105e4cbc6d28ae7b36bd5143493c
mmalak7 <marcinmalak7@gmail.com>
2020-10-25 13:01:44 +0100

    display 3 boxes using three fiber

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <span>React Three js</span>
+      </div>
+      <div className="menu">
+        <span>Cubes</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
